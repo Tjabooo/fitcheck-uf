ddb8036d2e6663ff65d0cbbd42074f9f802aaace
Tjabooo <frollegaming@hotmail.com>
2024-11-06 11:52:24 +0100

    Added home view and styling

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href='https://fonts.googleapis.com/css?family=Outfit' rel='stylesheet'>
+  <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}">
+  <title>FitCheck UF</title>
+  <style>
+  </style>
+</head>
+<body>
+    <header>
+        <h4> VÄLKOMMEN TILL</h4>
+        <h1>FITCHECK.UF</h1>
+    </header>
+</body>
+</html>
