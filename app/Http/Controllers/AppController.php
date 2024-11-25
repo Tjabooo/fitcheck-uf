4cc19bf13b73052fe440595d61603804054f2625
Tjabooo <frollegaming@hotmail.com>
2024-11-25 14:28:00 +0100

    fixed typo

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -13,6 +13,6 @@ class AppController extends Controller
 
     public function google_forms()
     {
-        return redirect()->away('https://docs.google.com/forms/d/e/1FAIpQLSeKU710LP3I_-ZCSGrINLt_lmIubLAriU7oYF9er5b_gQguQw/viewform?usp=sf_link')
+        return redirect()->away('https://docs.google.com/forms/d/e/1FAIpQLSeKU710LP3I_-ZCSGrINLt_lmIubLAriU7oYF9er5b_gQguQw/viewform?usp=sf_link');
     }
 }
